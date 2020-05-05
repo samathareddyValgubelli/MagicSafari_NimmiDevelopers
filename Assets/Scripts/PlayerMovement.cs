d07c3f73903550eed19ac633d9e0ba5eb11d8206
Aminul Khan <aminulhussain27@gmail.com>
2020-05-05 18:59:41 +0530

    Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Camera camera;
+    public bool isStopped = false;
+    Ray ray;
+    RaycastHit hit;
+
+    string playerTag = "Player";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isStopped)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                Debug.Log("Rayast clicked " + hit.collider.gameObject.name);
+                /// Name Comparison
+                if (hit.collider.tag.Equals(playerTag))
+                {
+                    isStopped = !isStopped;
+                }
+            }
+        }
+    }
+}
