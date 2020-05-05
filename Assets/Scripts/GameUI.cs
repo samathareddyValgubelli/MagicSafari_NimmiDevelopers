8703107a7f610ba5db1e2fb2625c3980316fac5e
samathareddyValgubelli <samathareddy.valgubelli@gmail.com>
2020-05-05 14:28:16 +0530

    Action selections and prefabs
    - Top hud prefab
    - Action selection and applying

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -1,18 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class GameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameUI instance = null;
+
+    // Game Instance Singleton
+    public static GameUI Instance
     {
-        
+        get
+        {
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        // if the singleton hasn't been initialized yet
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
     }
+
+    public enum enSelectedType
+    {
+        Gravity,
+        Destroy,
+        Shape,
+        Magic,
+        None
+    }
+
+    public enSelectedType selectedType;
+
+    private void Start()
+    {
+        OnClickTypeToApply(-1);
+    }
+
+    public void OnClickTypeToApply(int i)
+    {
+        switch(i)
+        {
+            case 0:
+                selectedType = enSelectedType.Destroy;
+                break;
+            case 1:
+                selectedType = enSelectedType.Gravity;
+                break;
+            case 2:
+                selectedType = enSelectedType.Shape;
+                break;
+            case 3:
+                selectedType = enSelectedType.Magic;
+                break;
+            default:
+                selectedType = enSelectedType.None;
+                break;
+        }
+    }
+   
 }
+
---
+++ Assets/Scripts/OnClickCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnClickCube : MonoBehaviour
+{
+    public UnityEvent anEvent;
+
+    private void OnMouseDown()
+    {
+        anEvent.Invoke();
+    }
+
+    Vector3 targetPosition = new Vector3(0, 1.5f, 20);
+    public void EventClick(GameObject obj)
+    {
+        Debug.Log("object name............" + obj.name);
+
+        if (GameUI.Instance.selectedType == GameUI.enSelectedType.Gravity)
+        {
+            if (obj.transform.position.x < 1f)
+            {
+                obj.GetComponent<Rigidbody>().useGravity = false;
+
+                //obj.transform.position = new Vector3(obj.transform.position.x, 1f, obj.transform.position.z);
+
+                obj.transform.position = Vector3.Lerp(transform.position, targetPosition, 0.5f * Time.deltaTime);
+
+            }
+        }
+    }
+}
